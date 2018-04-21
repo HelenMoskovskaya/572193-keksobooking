bf8a6118ecbda6f8cfdae9826010ef25e4b6219a
Helen Moskovskaya <helenmoskovskaya@gmail.com>
2018-04-21 08:58:55 +0300

    drag and drop

--- js/map.js
+++ js/map.js
@@ -134,7 +134,7 @@ var getAdvert = function () {
         title: OFFER_TITLES[getRandomValue(OFFER_TITLES)],
         address: xСoordinate + ',' + yСoordinate,
         price: getRandomNumber(MIN_PRICE, MAX_PRICE),
-        type: getrandomType(Object.keys(TYPES_OFFER)),
+        type: TYPES_OFFER[getrandomType(Object.keys(TYPES_OFFER))],
         rooms: getRandomNumber(1, ROOMS_QUANTITY),
         guests: getRandomNumber(1, GUESTS_QUANTITY),
         checkin: CHECKIN_OFFER[getRandomValue(CHECKIN_OFFER)],
@@ -175,8 +175,7 @@ var renderCard = function (advert) {
   cardElement.querySelector('.popup__title').textContent = advert.offer.title;
   cardElement.querySelector('.popup__text--address').textContent = advert.offer.address;
   cardElement.querySelector('.popup__text--price').textContent = advert.offer.price + '₽/ночь';
-
-  cardElement.querySelector('.popup__type').textContent = TYPES_OFFER[advert.offer.type];
+  cardElement.querySelector('.popup__type').textContent = advert.offer.type;
   var cardRooms;
   switch (advert.offer.rooms) {
     case 1:
@@ -215,6 +214,7 @@ advert.offer.rooms + ' ' + ' ' + cardRooms + ' ' + 'для' + ' ' + advert.offer
       photoElement.height = PHOTOS_HEIGHT;
       photoElement.alt = 'Фотография жилья';
       photoElement.src = advert.offer.photos[j];
+      photoElement.style.marginRight = '5' + 'px';
       photoList.appendChild(photoElement);
     }
     return photoElement;
@@ -329,6 +329,7 @@ var activatePage = function () {
   }
   inputAddress.value = getPinMainCoordinate();
   createPins(cards);
+  mapPinMain.addEventListener('mousedown', mapPinMainHandler);
 };
 
 var onButtonMainPinClick = function () {
@@ -448,7 +449,56 @@ var changeFieldsetForm = function () {
   resetButton.addEventListener('keydown', onButtonResetKeydown);
 };
 
+
+// Перемещение главного пина //
+
+var mapPinMainHandler = function (evt) {
+  evt.preventDefault();
+  var maxLeft = map.offsetWidth - MAP_PIN_MAIN_WIDTH;
+  var minLeft = 0;
+  var maxTop = map.offsetHeight - MAP_PIN_MAIN_HEIGHT;
+  var minTop = 0;
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+    var newTop = mapPinMain.offsetTop - shift.y;
+    var newLeft = mapPinMain.offsetLeft - shift.x;
+    if (newLeft < minLeft) {
+      newLeft = minLeft;
+    } else if (newLeft > maxLeft) {
+      newLeft = maxLeft;
+    }
+    if (newTop < minTop) {
+      newTop = minTop;
+    } else if (newTop > maxTop) {
+      newTop = maxTop;
+    }
+    mapPinMain.style.left = newLeft + 'px';
+    mapPinMain.style.top = newTop + 'px';
+  };
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+    onMouseMove(upEvt);
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+};
+
 // Вызов функций
+
 getMainButtonCoordinate();
 showPage();
 closeCard();
